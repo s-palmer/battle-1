3425324e84d397ab46cf1e94e9c9f8cbce188996
Sergei Palmer <zigpalmer@gmail.com>
2021-10-08 16:02:45 +0100

    player 2 can attack

--- app.rb
+++ app.rb
@@ -27,10 +27,15 @@ class Battle < Sinatra::Base
 
   get '/attack' do
     @game = $game
-    @game.attack(@game.player_2)
-    @game.switch_turns
+    @game.attack(@game.opponent_of(@game.current_turn))
     erb :attack
   end
+
+  post '/switch-turns' do
+    $game.switch_turns
+    redirect '/play'
+  end
+
 
   # post '/turn' do
   #   @game = $game
